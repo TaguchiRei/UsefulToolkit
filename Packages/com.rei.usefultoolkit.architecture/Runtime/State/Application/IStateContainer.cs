989c2e270c3e1c25db3166232709dd5d494c9676
reitaghchi <ren.len2426@gmail.com>
2026-07-23 19:09:43 +0900

    [add]IStateContainer改修

--- Packages/com.rei.usefultoolkit.architecture/Runtime/State/Application/IStateContainer.cs
+++ Packages/com.rei.usefultoolkit.architecture/Runtime/State/Application/IStateContainer.cs
@@ -5,7 +5,7 @@ namespace UsefulToolkit.Application.StateManagement
     /// <summary>
     /// ステートコンテナの全機能を保持するインターフェース
     /// </summary>
-    public interface IStateContainer : IStateContainerGetter, IStateContainerSetter, IStateContainerRegistration
+    public interface IStateContainer : IStateContainerGetter, IStateContainerRegistration
     {
     }
 
@@ -24,24 +24,6 @@ namespace UsefulToolkit.Application.StateManagement
     }
 
     /// <summary>
-    /// StateContainerにActionの登録をする際に利用する
-    /// </summary>
-    public interface IStateContainerSetter
-    {
-        /// <summary>
-        /// 特定のStateにイベントを登録する。
-        /// 戻り値のsubscriptionをDisposeすることで、登録者自身が解除できる。
-        /// </summary>
-        /// <param name="action">登録するアクション</param>
-        /// <param name="phase">どのタイミングで実行するか</param>
-        /// <param name="subscription">購読解除用のIDisposable</param>
-        /// <typeparam name="T">イベント登録を行う対象のState</typeparam>
-        /// <returns>登録できた場合はtrueを返す</returns>
-        public bool TryRegisterEvent<T>(Action<IStateContainerGetter> action, StatePhase phase, out IDisposable subscription)
-            where T : IStateGetter;
-    }
-
-    /// <summary>
     /// StateContainerにStateの登録、解除を行う
     /// </summary>
     public interface IStateContainerRegistration
@@ -50,11 +32,18 @@ namespace UsefulToolkit.Application.StateManagement
         /// Stateを登録する
         /// </summary>
         /// <param name="state">登録するState</param>
-        /// <typeparam name="TGetter">IStateGetterを継承したStateの取得インターフェース</typeparam>
-        /// <typeparam name="TState">登録するStateの型</typeparam>
         /// <returns>登録できた場合はtrueを返す(同一Getter型が登録済みの場合はfalse)</returns>
-        public bool TryRegisterState<TGetter, TState>(TState state)
-            where TGetter : IStateGetter
-            where TState : StateBase, TGetter;
+        public bool TryRegisterState<T>(T state)
+            where T : StateBase, IStateGetter;
+
+        public bool TryRegisterGameState<T>(T state)
+            where T : GameStateBase, IStateGetter;
+
+        public bool TryRegisterSceneState<T, TSceneEnum>(T state, TSceneEnum scene)
+            where T : SceneStateBase, IStateGetter
+            where TSceneEnum : Enum;
+
+        public bool TryRegisterUnRegistableState<T>(T state)
+            where T : UnRegistableStateBase, IStateGetter;
     }
 }
