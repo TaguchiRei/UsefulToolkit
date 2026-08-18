aacd3922f97f087138ed3eab8738600977da754a
reitaghchi <ren.len2426@gmail.com>
2026-08-18 14:20:27 +0900

    [add]Injectable作成

---
+++ Packages/com.rei.usefultoolkit.framework/Runtime/Utility/InitializePhase.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace UsefulToolkit.Framework.Initialization
+{
+    public enum InitializePhase
+    {
+        None,
+        Collection,
+        Inject,
+        Initialize,
+    }
+}
